d2faab2ee7f7e27c7a0536e6a0cca225c0a6cf36
aa362 <138415528+aa362@users.noreply.github.com>
2025-06-17 14:37:15 +0900

    Add files via upload

---
+++ test2.lua
@@ -0,0 +1,12 @@
+local screenGui = Instance.new("ScreenGui")
+screenGui.IgnoreGuiInset = true
+screenGui.ResetOnSpawn = false
+screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local loadingImage = Instance.new("ImageLabel")
+loadingImage.Size = UDim2.new(1, 0, 1, 0)
+loadingImage.Position = UDim2.new(0, 0, 0, 0)
+loadingImage.Image = "rbxassetid://77043026944368"
+loadingImage.BackgroundTransparency = 1
+loadingImage.ScaleType = Enum.ScaleType.Stretch
+loadingImage.Parent = screenGui
